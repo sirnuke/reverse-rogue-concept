7c828cdda3a10cf36e4e10fde813e21f9537491b
Bryan DeGrendel <bryan@degrendel.com>
2020-02-16 22:44:58 -0500

    Add placeholder Zircon display

---
+++ reverse-rogue-zircon/src/main/kotlin/com/degrendel/reverserogue/zircon/Application.kt
@@ -0,0 +1,34 @@
+package com.degrendel.reverserogue.zircon
+
+import org.hexworks.zircon.api.ColorThemes
+import org.hexworks.zircon.api.Components
+import org.hexworks.zircon.api.SwingApplications
+import org.hexworks.zircon.api.data.Size
+import org.hexworks.zircon.api.extensions.box
+import org.hexworks.zircon.api.screen.Screen
+import org.hexworks.zircon.api.uievent.MouseEventType
+import java.util.concurrent.locks.Condition
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
+
+class Application(private val lock: ReentrantLock, private val condition: Condition)
+{
+  fun launch()
+  {
+    val tileGrid = SwingApplications.startTileGrid()
+    val screen = Screen.create(tileGrid)
+    val panel = Components.panel()
+        .withSize(Size.create(4, 5))
+        .withDecorations(box())
+        .build()
+
+    screen.addComponent(panel)
+
+    panel.processMouseEvents(MouseEventType.MOUSE_MOVED) { event, _ -> println(event) }
+
+    screen.display()
+    screen.theme = ColorThemes.adriftInDreams()
+
+    screen.onShutdown { lock.withLock { condition.signal() } }
+  }
+}
--- reverse-rogue-zircon/src/main/kotlin/com/degrendel/reverserogue/zircon/Main.kt
+++ reverse-rogue-zircon/src/main/kotlin/com/degrendel/reverserogue/zircon/Main.kt
@@ -3,14 +3,21 @@ package com.degrendel.reverserogue.zircon
 import picocli.CommandLine
 import picocli.CommandLine.Command
 import java.util.concurrent.Callable
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 import kotlin.system.exitProcess
 
 @Command(name = "ReverseRogue", mixinStandardHelpOptions = true)
-class Main() : Callable<Int>
+class Main : Callable<Int>
 {
+  private val lock = ReentrantLock()
+  private val condition = lock.newCondition()
+
   override fun call(): Int
   {
-    return 0;
+    Application(lock, condition).launch()
+    lock.withLock { condition.await() }
+    return 0
   }
 }
 
