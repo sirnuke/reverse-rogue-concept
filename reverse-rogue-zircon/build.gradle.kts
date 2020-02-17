117ba4ae0a160e51fc29244a2fc72cb7410c8edc
Bryan DeGrendel <bryan@degrendel.com>
2020-02-16 21:35:15 -0500

    Add Zircon dependencies

---
+++ reverse-rogue-zircon/build.gradle.kts
@@ -0,0 +1,10 @@
+repositories {
+    maven {
+      url = uri("https://jitpack.io")
+    }
+}
+
+dependencies {
+    implementation("org.hexworks.zircon:zircon.core-jvm:2020.0.1-PREVIEW")
+    implementation("org.hexworks.zircon:zircon.jvm.swing:2020.0.1-PREVIEW")
+}
