8c821003c117bb39cb19e55a66b17e88220f653d
Bryan DeGrendel <bryan@degrendel.com>
2020-02-23 22:20:57 -0500

    Add component for tracking room dimensions

--- reverse-rogue-common/src/main/kotlin/com/degrendel/reverserogue/common/components/Common.kt
+++ reverse-rogue-common/src/main/kotlin/com/degrendel/reverserogue/common/components/Common.kt
@@ -1,7 +1,6 @@
 package com.degrendel.reverserogue.common.components
 
 import com.badlogic.ashley.core.Component
-import com.degrendel.reverserogue.common.Cardinal
 import com.degrendel.reverserogue.common.Position
 import com.degrendel.reverserogue.common.SquareType
 import com.degrendel.reverserogue.common.WallOrientation
@@ -17,9 +16,9 @@ data class PositionComponent(val position: Position) : Component
 
 /** Indicates this entity is a square in the level. */
 data class SquareTypeComponent(val type: SquareType) : Component
-{
-}
 
+/** Records the orientation of this wall square. */
 data class WallOrientationComponent(val wallOrientation: WallOrientation) : Component
-{
-}
+
+/** Tracks the id and dimensions of the room. */
+data class RoomComponent(val id: Int, val width: Int, val height: Int) : Component
--- reverse-rogue-common/src/main/kotlin/com/degrendel/reverserogue/common/components/ComponentMaps.kt
+++ reverse-rogue-common/src/main/kotlin/com/degrendel/reverserogue/common/components/ComponentMaps.kt
@@ -8,8 +8,10 @@ object ComponentMaps
   val position: ComponentMapper<PositionComponent> = ComponentMapper.getFor(PositionComponent::class.java)
   val squareType: ComponentMapper<SquareTypeComponent> = ComponentMapper.getFor(SquareTypeComponent::class.java)
   val wallOrientation: ComponentMapper<WallOrientationComponent> = ComponentMapper.getFor(WallOrientationComponent::class.java)
+  val roomData: ComponentMapper<RoomComponent> = ComponentMapper.getFor(RoomComponent::class.java)
 }
 
 fun Entity.getPosition() = ComponentMaps.position.get(this).position
 fun Entity.getSquareType() = ComponentMaps.squareType.get(this).type
 fun Entity.getWallOrientation() = ComponentMaps.wallOrientation.get(this).wallOrientation
+fun Entity.getRoomData() = ComponentMaps.roomData.get(this)!!
