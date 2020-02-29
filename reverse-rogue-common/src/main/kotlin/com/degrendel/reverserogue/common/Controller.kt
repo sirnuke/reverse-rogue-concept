a2befa73be69cc88ca8005a61efac222610c376f
Bryan DeGrendel <bryan@degrendel.com>
2020-02-29 01:46:25 -0500

    Add better computation of simple AI weights
    Each behavior has a relative weight, and the overall AI has an optional min/max weight.

--- reverse-rogue-common/src/main/kotlin/com/degrendel/reverserogue/common/Controller.kt
+++ reverse-rogue-common/src/main/kotlin/com/degrendel/reverserogue/common/Controller.kt
@@ -2,13 +2,12 @@ package com.degrendel.reverserogue.common
 
 sealed class Controller
 object PlayerControlled : Controller()
-data class SimpleAI(val behaviors: List<Behavior>): Controller()
+data class SimpleAI(val behaviors: List<Behavior>, val weightMin: Int = 0, val weightMax: Int = 0): Controller()
 data class AgentAI(val agent: SoarAgent) : Controller()
 
 sealed class Behavior
 {
-  abstract val weightMin: Int
-  abstract val weightMax: Int
+  abstract val weight: Int
 }
-data class MoveTowardsRogue(override val weightMin: Int = 0, override val weightMax: Int = 0): Behavior()
-data class MoveTowardsConjurer(override val weightMin: Int = 0, override val weightMax: Int = 0): Behavior()
+data class MoveTowardsRogue(override val weight: Int = 0): Behavior()
+data class MoveTowardsConjurer(override val weight: Int = 0): Behavior()
